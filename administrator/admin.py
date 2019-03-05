d165831d62556466d5a4980b8ac604a30c4a5869
David <slamvsem@gmail.com>
2019-03-05 22:05:36 +0700

    add models

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,3 +1,76 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "lk_code",
+        "subscribe",
+        "last_news",
+    )
+admin.site.register(Account, AccountAdmin)
+
+class DirectionAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "faculty",
+        "keys_plus",
+        "ball_k",
+        "ball_b",
+        "url",
+        "active",
+        "description",
+        "profile_name",
+    )
+admin.site.register(Direction, DirectionAdmin)
+
+class MsgAdmin(admin.ModelAdmin):
+    list_display=(
+        "id",
+        "pay",
+        "msg"
+    )
+
+admin.site.register(Msg, MsgAdmin)
+
+class SphereAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+admin.site.register(Sphere, SphereAdmin)
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+admin.site.register(Subject, SubjectAdmin)
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        "answer",
+        "reaction",
+        "is_true"
+    )
+
+admin.site.register(Answer, AnswerAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "question",
+        "answers"
+    )
+
+admin.site.register(Question, QuestionAdmin)
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "updated_date",
+        "start_date"
+    )
+
+admin.site.register(Test, TestAdmin)
--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,101 @@
 from django.db import models
 
-# Create your models here.
+class Sphere(models.Model):
+    """Сфера"""
+    
+    name = models.CharField(max_length = 100, verbose_name = "Название")
+
+    class Meta:
+        verbose_name        = "Сфера"
+        verbose_name_plural = "Сферы"
+
+
+class Subject(models.Model):
+    """Предмет"""
+
+    name = models.CharField(max_length = 100, verbose_name = "Название")
+
+    class Meta:
+        verbose_name        = "Предмет"
+        verbose_name_plural = "Предметы"
+
+class Direction(models.Model):
+    """Направления"""
+    name            = models.CharField(max_length = 50, verbose_name = "Название")
+    faculty         = models.CharField(max_length = 150, verbose_name = "Факультет")
+    keys_plus       = models.CharField(max_length = 15, verbose_name = "Код направления")
+    ball_k          = models.IntegerField(verbose_name="Минимальный балл на контракт")
+    ball_b          = models.IntegerField(verbose_name="Минимальный балл на бюджет", null = True)
+    url             = models.URLField(verbose_name="Ссылка на направление")
+    active          = models.BooleanField(default=True, verbose_name="Активно")
+    description     = models.TextField(verbose_name="Описание")
+    profile_name    = models.CharField(max_length = 50, verbose_name = "Профиль")
+    spheres         = models.ManyToManyField(Sphere, verbose_name = "Сферы", related_name= "direction", blank = True)
+    subjects        = models.ManyToManyField(Subject, verbose_name = "Предметы", related_name= "direction", blank = True)
+
+    class Meta:
+        verbose_name        = "Направление"
+        verbose_name_plural = "Направления"
+
+class Account(models.Model):
+    """Аккаунт пользователя"""
+
+    id              = models.IntegerField(verbose_name="id", primary_key=True)
+    lk_code         = models.IntegerField(verbose_name="Код личного кабинета")
+    subscribe       = models.BooleanField(default=False, verbose_name="Подписка")
+    last_news       = models.CharField(max_length = 100, verbose_name = "Последняя новость")
+    spheres         = models.ManyToManyField(Sphere, verbose_name = "Сферы", related_name= "account", blank = True)
+    subjects        = models.ManyToManyField(Subject, verbose_name = "Предметы", related_name= "account", blank = True)
+
+    class Meta:
+        verbose_name        = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+class Msg(models.Model):
+    """Сообщение"""
+
+    pay = models.CharField(max_length = 50, verbose_name = "payload")
+    msg = models.TextField(verbose_name="Сообщение")
+    
+    class Meta:
+        verbose_name        = "Сообщение"
+        verbose_name_plural = "Сообщения"
+
+class Answer(models.Model):
+    """Ответ"""
+
+    answer      = models.TextField(verbose_name="Ответ")
+    reaction    = models.TextField(verbose_name="Реакция на ответ")
+    is_true     = models.BooleanField(verbose_name="Верный ответ")
+
+    class Meta:
+        verbose_name        = "Ответ"
+        verbose_name_plural = "Ответы"
+
+
+
+class Question(models.Model):
+    """Вопрос"""
+
+    question    = models.TextField(verbose_name="Вопрос")
+    answers     = models.ForeignKey(Answer, on_delete = models.CASCADE, verbose_name = "Ответы", related_name="question")
+
+    class Meta:
+        verbose_name        = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+class Test(models.Model):
+    """Тест"""
+
+    name            = models.CharField(max_length = 50, verbose_name = "Название")
+    updated_date    = models.DateTimeField(verbose_name = "Дата и время изменения", auto_now = True)
+    start_date      = models.DateField(verbose_name="Дата начала теста")
+    questions       = models.ManyToManyField(Question, verbose_name = "Вопросы", related_name="test", blank = True)
+
+    class Meta:
+        verbose_name        = "Тест"
+        verbose_name_plural = "Тесты"
+
+
+
+
