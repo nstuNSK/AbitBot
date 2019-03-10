9dd1b234018383b593074d7e7551728dc45e517f
David <slamvsem@gmail.com>
2019-03-10 15:09:09 +0700

    fix

--- bot/bot.py
+++ bot/bot.py
@@ -27,11 +27,11 @@ def search_direction(user, type):
     if type == "SPHERE":
         spheres = user.spheres.all()
         for sphere in spheres:
-            dir = dir + sphere.direction.all()
+            dir = dir + sphere.direction.all().values()
     elif type == "SUBJECTS":
         subjects = user.subjects.all()
         for subject in subjects:
-            dir = dir + subject.direction.all()
+            dir = dir + subject.direction.all().values()
     if len(dir)!=0:
         user.random_id = user.random_id + 1
         user.save()
