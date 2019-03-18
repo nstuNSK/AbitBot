8d6dfc52b65990c77289207b2a4bf447d9ededb3
Хван Дмитрий Максимович <xvan.2016@stud.nstu.ru>
2019-03-18 13:22:54 +0700

    tests

--- bot/bot.py
+++ bot/bot.py
@@ -88,7 +88,7 @@ def get_questions(pay,user):
     #try:
     question = Question.objects.get(id = id)
     print(question.question)
-    vk.method("messages.send", {"random_id": user.random_id, "user_id": id, "message": question.question, "keyboard": keyboards.get_question_keyboard(question = question)})
+    vk.method("messages.send", {"random_id": user.random_id, "user_id": id, "message": str(question.question), "keyboard": keyboards.get_question_keyboard(question = question)})
         #return True
     #except:
     #    return False
