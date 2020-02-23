c6951710ecef7db6b6de1a41588df284002d49ff
Хван Дмитрий Максимович <drestbm@gmail.com>
2020-02-23 14:18:18 +0700

    front-end markup

--- markup/urls.py
+++ markup/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from markup.views import *
 
 urlpatterns = [
+    path('', index),
     path('add_class/', Class_Add.as_view()),
     path('get_sample/', Get_Questions.as_view())
 ]
--- markup/views.py
+++ markup/views.py
@@ -10,12 +10,17 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from django.contrib.staticfiles import finders
+from django.template import Context, loader
 
 from AbitBot import settings
 import pandas as pd
 
 import random
 
+def index(request):
+	if request.method == "GET":
+	    template=loader.get_template("index.html")
+	    return HttpResponse(template.render())
 
 class Class_Add(APIView):
     permission_classes = (AllowAny,)
