c6fea74c2adbb7b7f2cc368fbffab481156beeb5
ProgrammerOfMoms <david.tohunts@gmail.com>
2020-03-01 18:02:38 +0700

    added url for login

--- markup/urls.py
+++ markup/urls.py
@@ -4,5 +4,6 @@ from markup.views import *
 urlpatterns = [
     path('', index),
     path('add_class/', Class_Add.as_view()),
-    path('get_sample/', Get_Questions.as_view())
+    path('get_sample/', Get_Questions.as_view()),
+    path('login/', LoginView.as_view())
 ]
