2df61fd2c0a4a3f6cf77a746697872756e7c6431
ProgrammerOfMoms <david.tohunts@gmail.com>
2020-03-02 15:44:21 +0700

    added csv path

--- AbitBot/deploy_settings.py
+++ AbitBot/deploy_settings.py
@@ -1,5 +1,6 @@
 API_KEY = "s13dfget456DADHGWEv34g435f"
 SECRET_DB = "dbsvlJKLnlkHVGKKkhjkhCCYICyyiIYVGTIG"
+DATA_CSV_PATH = "/home/Main/AbitBot/static/files/qa.csv"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/Main/AbitBot/static'
