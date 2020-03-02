c795f58d7a0235efd66c945256757fa250ede4d9
ProgrammerOfMoms <david.tohunts@gmail.com>
2020-03-02 09:55:15 +0700

    test

---
+++ AbitBot/deploy_settings.py
@@ -0,0 +1,6 @@
+API_KEY = "s13dfget456DADHGWEv34g435f"
+SECRET_DB = "dbsvlJKLnlkHVGKKkhjkhCCYICyyiIYVGTIG"
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/Main/AbitBot/static'
+MEDIA_ROOT = '/home/Main/AbitBot/media'
