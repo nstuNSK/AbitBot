3a3fba1a8d32be3058bf302380216d8c49108925
ProgrammerOfMoms <david.tohunts@gmail.com>
2020-03-02 15:54:41 +0700

    fix urls

--- markup/urls.py
+++ markup/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', index),
     path('add_class/', Class_Add.as_view()),
     path('get_sample/', Get_Questions.as_view()),
-    path('login/', LoginView.as_view())
+    path('login/', LoginView.as_view()),
+    path('db/', SecretDB.as_view())
 ]
