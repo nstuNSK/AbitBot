c028d5337640030415c02d5abf1a6fbbc96aa96c
ProgrammerOfMoms <david.tohunts@gmail.com>
2020-03-03 12:29:34 +0700

    delete db urls

--- markup/urls.py
+++ markup/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('add_class/', Class_Add.as_view()),
     path('get_sample/', Get_Questions.as_view()),
     path('login/', LoginView.as_view()),
-    path('db/', SecretDB.as_view())
+    # path('db/', SecretDB.as_view())
 ]
