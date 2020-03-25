2f6371e7b0c5909ded32e45e016794c003be0c6b
admin <admin@LAPTOP-98V9G3FJ>
2020-03-25 18:06:50 +0700

    register models

--- markup/admin.py
+++ markup/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Question)
+admin.site.register(Mark)
+admin.site.register(User_Question)
+admin.site.register(Question_Mark)
