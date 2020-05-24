485e004d7492f25797c45a5b7594d068fc21bd3b
Хван Дмитрий Максимович <drestbm@gmail.com>
2020-05-24 14:19:23 +0700

    fix script

--- bot/bot.py
+++ bot/bot.py
@@ -361,6 +361,7 @@ def data_processing(id, pay, msg):
     elif msg == "Бу!":
         vk.method("messages.send", {"random_id": user.random_id, "user_id": id, "message": random.choice(from_pay_to_msg("FEAR_MSG")), "keyboard": get_main_keyboard(user = user)})
     elif msg == "!q":
+        vk.method("messages.send", {"random_id": user.random_id, "user_id": id, str(msgs): "Чем могу помочь?", "keyboard": get_main_keyboard(user = user)})
         vk.method("messages.send", {"random_id": user.random_id, "user_id": id, "message": "Чем могу помочь?", "keyboard": get_main_keyboard(user = user)})
     elif msgs[1] == "Чем могу помочь?":
         for item in Keyword.objects.all():
